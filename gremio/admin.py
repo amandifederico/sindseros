6e12147d032cd8d4e42b3fcd347ba2382ed59c67
Fede amandi <amandifederico@gmail.com>
2014-09-19 09:49:19 -0300

    agregados campos al admin

--- gremio/admin.py
+++ gremio/admin.py
@@ -7,11 +7,14 @@ class LocalidadAdmin(admin.ModelAdmin):
 
 class FamiliaAdmin(admin.ModelAdmin):
     raw_id_fields = ('afiliado',)
+    list_display = ['nombre','parentesco','documento','estado','afiliado']
+    search_fields = ['nombre','estado']
     pass
 
 class AfiliadoAdmin(admin.ModelAdmin):
     raw_id_fields = ('ciudad',)
     list_display = ['apellido','nombre','lugar_empleo','documento','direccion','sexo','e_civil','telefono','celular','e_mail','fecha_nac','ciudad']
+    search_fields = ['nombre','apellido','documento']
     pass
 
 admin.site.register(Localidad, LocalidadAdmin)
